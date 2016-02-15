afcc1ab8661a455525d12b59e53187c389b1c261
Matt Otis Kennedy <mattotiskennedy@gmail.com>
2016-02-15 10:28:10 -0600

    added table

--- doggiedaycare/config/routes.rb
+++ doggiedaycare/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :dogs, only: [:index]
+
   get 'pages/index'
+
+  get '/about' => 'pages#about'
 
   get 'pages/about'
 
@@ -8,7 +12,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+root 'pages#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
